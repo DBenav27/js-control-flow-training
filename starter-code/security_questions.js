47d5da72a96e2bf60ef9ca6328f1985d0b6967e6
Daniel Benavidez <danielbenavidez27@gmail.com>
2017-08-17 13:26:10 -0500

    finished security question and answer

--- starter-code/security_questions.js
+++ starter-code/security_questions.js
@@ -1,1 +1,32 @@
 console.log("security_questions.js loaded");
+
+var securityQuestions = [
+  {
+    question: "What is your pet's name?",
+    expectedAnswer: "Tebow"
+  },
+  {
+    question: "What is your favorite NBA team?",
+    expectedAnswer: "Spurs"
+  },
+  {
+    question: "What state were you born in?",
+    expectedAnswer: "California"
+  }
+  ]
+
+var userAnswer = "";
+var hacker = false;
+
+for (var i=0; i < securityQuestions.length; i++){
+	userAnswer = prompt(securityQuestions[i].question);
+	if (userAnswer !== securityQuestions[i].expectedAnswer){
+		alert("Incorrect answer!");
+    hacker = true;
+		break;
+	}
+}
+
+if (hacker) {
+  console.log("Stop!");
+}
